1caec11e5445c9566e8fcad106328222b12399bd
nha-blip <Nhavotan2k6@gmail.com>
2025-07-20 20:08:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+//bài 1
 function nguyento(n){
-    if(n<3) console.log("true") 
+    if(n<2) console.log("false") 
         else{
             let i=2;
             while(i<=Math.sqrt(n)){
@@ -12,6 +13,7 @@ function nguyento(n){
             console.log("true"); 
     }  
 }
+//bài 2
 function sum(n){
     let i=1,sum=0
     while(i<n){
@@ -20,6 +22,7 @@ function sum(n){
     }
     console.log(sum)
 }
+//bài 3
 let arr=[1,2,3,4,5,6,7,8,9,10];
 function array(arr){
     let New = arr.filter(x=>x>0)
